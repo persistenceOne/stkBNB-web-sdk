ab817bc01fa8060a3cef1bb1e85f4d4f86f6b039
Damir Bajramovic <damir.bajramovic@blc.edu.ba>
2023-06-15 14:50:18 +0200

    Add V2 features. (#5)
    Adding V2 features for the stkBNB contracts.
    - `instantClaim` (the users are able to claim their requests instantly,
    for a fee)
    - `automatedClaim` (EIP712 message signing, for gasless claims -
    deducted from the user's balance)
    - checking the unlock period in the contract configuration
    - checking if `instantClaim` is possible

---
+++ src/eip712-utils.ts
@@ -0,0 +1,26 @@
+import { BigNumber, TypedDataDomain } from "ethers";
+import { MAINNET_CONFIG, TESTNET_CONFIG } from "./stkbnb-web-sdk";
+
+export const StakePoolDomainTestnet: TypedDataDomain = {
+  name: "Stake Pool",
+  version: "v2",
+  chainId: 97, // BSC testnet
+  verifyingContract: TESTNET_CONFIG.stakePool
+}
+
+export const StakePoolDomainMainnet: TypedDataDomain = {
+  name: "Stake Pool",
+  version: "v2",
+  chainId: 56, // BSC mainnet 
+  verifyingContract: MAINNET_CONFIG.stakePool
+}
+
+export const ClaimDataType = {
+  Claim: [
+    { name: "index", type: "uint256" },
+  ]
+};
+
+export type ClaimArgs = {
+  index: BigNumber
+}
